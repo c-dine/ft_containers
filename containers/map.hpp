73ca2a97be6a8ce7c4e36b748d6a9e717312de1a
thedine99 <celine.dine@yahoo.fr>
2022-08-20 22:33:02 +0200

    value compare

--- containers/map.hpp
+++ containers/map.hpp
@@ -44,6 +44,22 @@ template<
 		// typedef typename Rep_type::size_type              						size_type;
 		// typedef typename Rep_type::difference_type        						difference_type;
 
+		class value_compare
+		{   // in C++98, it is required to inherit binary_function<value_type,value_type,bool>
+		friend class map;
+		protected:
+		Compare comp;
+		value_compare (Compare c) : comp(c) {}  // constructed with map's comparison object
+		public:
+		typedef bool result_type;
+		typedef value_type first_argument_type;
+		typedef value_type second_argument_type;
+		bool operator() (const value_type& x, const value_type& y) const
+		{
+			return comp(x.first, y.first);
+		}
+		}
+
 	private:
 		Rep_type			_tree;
 
